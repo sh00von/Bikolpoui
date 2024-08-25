ec370d380572bcf3ed9af72a13fa6da2412ccecb
sh0von <minar.svn@gmail.com>
2024-08-25 19:34:07 +0600

    Analytics

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
-
+import { Analytics } from "@vercel/analytics/react";
 const Layout = ({ children, title = 'Default Title', description = 'Default description', ogImage = '/path/to/default-image.jpg' }) => {
   return (
     <div className="flex flex-col min-h-screen bg-base-100">
@@ -21,6 +21,8 @@ const Layout = ({ children, title = 'Default Title', description = 'Default desc
       </Head>
       <Header />
       <main className="flex-grow">{children}</main>
+      
+      <Analytics />
       <Footer />
     </div>
   );
